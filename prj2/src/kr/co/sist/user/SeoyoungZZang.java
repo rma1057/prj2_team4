17b1a5fb0eb9b4d0ac7f9e9e7e192609adf148ba
owner <owner@DESKTOP-VK3LDBQ>
2019-08-28 11:34:55 +0900

    깃사용 연습입니다!

--- prj2/src/kr/co/sist/user/SeoyoungZZang.java
+++ prj2/src/kr/co/sist/user/SeoyoungZZang.java
@@ -1,9 +1,13 @@
 package kr.co.sist.user;
 
+/**
+ * 여러분 화이팅 서영최고 짱 
+ * @author owner
+ */
 public class SeoyoungZZang {
 
 	public static void main(String[] args) {
+		System.out.println("깃사용연습~~~");
+	}//main
 
-	}
-
-}
+}//class
